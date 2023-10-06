1b14657a67bceb38dd6d3d8a5d69bb5e572925bd
CryptoSnowPrince <topdirector2017@gmail.com>
2023-10-06 18:09:46 +0900

    reconfig frontend

--- frontend/README.md
+++ frontend/README.md
@@ -6,9 +6,10 @@
 OS: Ubuntu 22.04.3 LTS/Windows 10
 npm: 8.19.4
 node: v16.20.2
+yarn: 1.22.19
 ```
 
 ```shell
-npm install
-npm run dev
+yarn
+yarn dev
 ```
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false, net: false, tls: false }
-    return config
-  },
-}
+const nextConfig = {
+	reactStrictMode: true,
+};
+
+module.exports = nextConfig;
