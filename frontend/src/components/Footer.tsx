54b67d656b3217d0a5f6dae6ec9dbe5c6b99c57c
CryptoSnowPrince <topdirector2017@gmail.com>
2023-10-10 21:25:07 +0200

    update footer with feedback

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Footer() {
     return (
         <footer className='w-full'>
-            <div className='lg:w-2/3 w-11/12 flex flex-row text-white items-center justify-between mx-auto my-5'>
+            {/* <div className='lg:w-2/3 w-11/12 flex flex-row text-white items-center justify-between mx-auto my-5'>
                 <div className="flex flex-col text-center">
                     <Image src='/gitbook.svg' width={240} height={240} alt='logo' className="lg:w-[220px] w-[70px] my-auto" />
                     <h1 className="lg:text-lg text-sm">Gitbook</h1>
@@ -24,17 +24,17 @@ export default function Footer() {
                     <Image src='/dextools.svg' width={240} height={240} alt='logo' className="lg:w-[220px] w-[70px] my-auto" />
                     <h1 className="lg:text-lg text-sm">Dextools</h1>
                 </div>
-            </div >
+            </div > */}
             <div className='w-full text-white items-center px-5 flex flex-row my-5'>
                 <div className="lg:w-1/3 w-2/3 flex flex-row text-center gap-2">
                     <Image src='/logo.svg' width={80} height={80} alt='logo' className="lg:w-[80px] w-[40px] my-auto" />
                     <h1 className="lg:text-lg text-sm my-auto">2023 By Fast Meal Team</h1>
                 </div>
                 <div className="flex flex-row w-1/3 lg:gap-3 gap-1 lg:justify-center justify-end">
-                    <Image src='/instagram_white.svg' width={50} height={50} alt='logo' className="lg:w-[50px] w-[30px] my-auto" />
-                    <Image src='/youtube_white.svg' width={50} height={50} alt='logo' className="lg:w-[50px] w-[30px] my-auto" />
-                    <Image src='/telegram_white.svg' width={50} height={50} alt='logo' className="lg:w-[50px] w-[30px] my-auto" />
-                    <Image src='/x_white.svg' width={50} height={50} alt='logo' className="lg:w-[50px] w-[30px] my-auto" />
+                    <Image src='/instagram_white.svg' width={40} height={40} alt='logo' className="lg:w-[40px] w-[25px] my-auto" />
+                    <Image src='/youtube_white.svg' width={40} height={40} alt='logo' className="lg:w-[40px] w-[25px] my-auto" />
+                    <Image src='/telegram_white.svg' width={40} height={40} alt='logo' className="lg:w-[40px] w-[25px] my-auto" />
+                    <Image src='/x_white.svg' width={40} height={40} alt='logo' className="lg:w-[40px] w-[25px] my-auto" />
                 </div>
             </div >
         </footer >
